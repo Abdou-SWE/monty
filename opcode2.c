04f1d9f2a3938378e30ec77be98ab617ebe8e57b
Abdou-SWE <abdoulc434@gmail.com>
2024-01-18 13:14:43 -0800

    23

---
+++ opcode2.c
@@ -0,0 +1,144 @@
+#include "monty.h"
+
+/**
+ * _mod - computes the rest of the division of the second element
+ * by the top element of the stack
+ * @doubly: head of the linked list
+ * @cline: line number;
+ * Return: no return
+ * info:The result is stored in 2nd value node from the top
+ * and top value removed.
+ */
+
+void _mod(stack_t **doubly, unsigned int cline)
+{
+	int m = 0;
+	stack_t *aux = NULL;
+
+	aux = *doubly;
+
+	for (; aux != NULL; aux = aux->next, m++)
+		;
+
+	if (m < 2)
+	{
+		dprintf(2, "L%u: can't mod, stack too short\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*doubly)->n == 0)
+	{
+		dprintf(2, "L%u: division by zero\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+
+	aux = (*doubly)->next;
+	aux->n %= (*doubly)->n;
+	_pop(doubly, cline);
+}
+/**
+ * _pstr - prints the string of the stack
+ *
+ * @doubly: head of the linked list
+ * @cline: line number;
+ * Return: no return
+ */
+void _pstr(stack_t **doubly, unsigned int cline)
+{
+	stack_t *aux;
+	(void)cline;
+
+	aux = *doubly;
+
+	while (aux && aux->n > 0 && aux->n < 128)
+	{
+		printf("%c", aux->n);
+		aux = aux->next;
+	}
+
+	printf("\n");
+}
+/**
+ * _pchar - print the char value of the first element
+ *
+ * @doubly: head of the linked list
+ * @cline: line number;
+ * Return: no return
+ */
+void _pchar(stack_t **doubly, unsigned int cline)
+{
+	if (doubly == NULL || *doubly == NULL)
+	{
+		dprintf(2, "L%u: can't pchar, stack empty\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	if ((*doubly)->n < 0 || (*doubly)->n >= 128)
+	{
+		dprintf(2, "L%u: can't pchar, value out of range\n", cline);
+		free_vglo();
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", (*doubly)->n);
+}
+
+
+/**
+ * _rotl - rotates 1st element to the bottom and 2nd to the top
+ * @doubly: linked list head
+ * @cline: line nummber
+ */
+void _rotl(stack_t **doubly, unsigned int cline)
+{
+	stack_t *aux1 = NULL;
+	stack_t *aux2 = NULL;
+	(void)cline;
+
+	if (*doubly == NULL)
+		return;
+
+	if ((*doubly)->next == NULL)
+		return;
+
+	aux1 = (*doubly)->next;
+	aux2 = *doubly;
+
+	for (; aux2->next != NULL; aux2 = aux2->next)
+		;
+
+	aux1->prev = NULL;
+	aux2->next = *doubly;
+	(*doubly)->next = NULL;
+	(*doubly)->prev = aux2;
+	*doubly = aux1;
+}
+
+/**
+ * _rotr - reverse stack
+ * @doubly: linked list head
+ * @cline: line number
+ */
+void _rotr(stack_t **doubly, unsigned int cline)
+{
+	stack_t *aux = NULL;
+	(void)cline;
+
+	if (*doubly == NULL)
+		return;
+
+	if ((*doubly)->next == NULL)
+		return;
+
+	aux = *doubly;
+
+	for (; aux->next != NULL; aux = aux->next)
+		;
+
+	aux->prev->next = NULL;
+	aux->next = *doubly;
+	aux->prev = NULL;
+	(*doubly)->prev = aux;
+	*doubly = aux;
+}
